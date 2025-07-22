97b80574de9e897408b25d15ab88366fda12c33c
mqrcd <m.durufle@gmail.com>
2025-07-22 19:46:33 +0200

    users intégrés à la base SQLite

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const sequelize = require("./db");
 const Task = require("./models/Task"); // MDU ici je précise que la création de Task par exemple (app.post dans ce fichier) est liée avec le model Task.js
+const User = require("./models/User");
 
 const app = express();
 
@@ -14,7 +15,7 @@ sequelize.sync().then(() => {
 });
 // sequelize.sync({ force: true }); // pour refresh le sync quand je teste; à changer par le code du dessus quand tout est OK.
 
-// Exemples de routes
+// MDU : ROUTES VERS LES TÂCHES/TASKS
 app.get("/tasks", async (req, res) => {
   const tasks = await Task.findAll();
   res.json(tasks);
@@ -75,5 +76,44 @@ app.delete("/tasks/:id", async (req, res) => {
     res.status(500).json({ message: "Erreur serveur", error });
   }
 }); // ==>> MDU: sans ca il était impossible supprimer les tâches
+
+// MDU : FIN ROUTES VERS LES TÂCHES/TASKS
+
+// MDU : ROUTES VERS LES USERS refaites à la main by myself en suivant la logique que j'avais trouvé avec de l'aide pour les tasks ci-dessus
+app.get("/users", async (req, res) => {
+  const users = await User.findAll();
+  res.json(users);
+});
+
+app.get("/users:id", async (req, res) => {
+  try {
+    const user = await User.findByPk(req.params.id);
+    if (!user) {
+      return res.status(404).json({ message: "Tâche non trouvée" });
+    }
+
+    res.json(user);
+  } catch (error) {
+    console.error("Erreur récupération tâche :", error);
+    res.status(500).json({ message: "Erreur serveur", error });
+  }
+});
+
+app.get("/users/auth", async (req, res) => {
+  const { login, password } = req.query;
+
+  try {
+    const user = await User.findOne({ where: { login, password } });
+
+    if (!user) {
+      return res.status(401).json({ message: "Identifiants invalides" });
+    }
+
+    res.json(user);
+  } catch (error) {
+    console.error("Erreur lors de l'authentification :", error);
+    res.status(500).json({ message: "Erreur serveur", error });
+  }
+});
 
 app.listen(3000, () => console.log("API lancée sur http://localhost:3000"));
---
+++ models/User.js
@@ -0,0 +1,14 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../db");
+
+const User = sequelize.define("User", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  login: { type: DataTypes.STRING, allowNull: false },
+  password: { type: DataTypes.STRING, allowNull: false },
+});
+
+module.exports = User;
